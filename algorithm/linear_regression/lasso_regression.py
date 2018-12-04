3d5ceced2858fb49da1776074cad1cd65c8c5d84
quyang <quyang@focusmedia.cn>
2018-12-04 18:57:44 +0800

    2018-12-04 提交了lasso和ridge

--- algorithm/linear_regression/lasso_regression.py
+++ algorithm/linear_regression/lasso_regression.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import Lasso
 from sklearn.linear_model import SGDRegressor
 
 __author__ = 'gloom'
-"""这个类是栅格搜索，他们只是来源于不同的两个类，这个方法主要是为了能够在lasso和岭回归中选择到最好方法和参数"""
+"""这个是使用lasso进行回归，L1正则化"""
 def main():
 
     X = 2 * np.random.rand(100, 1)
@@ -13,12 +13,12 @@ def main():
 
     lasso_reg = Lasso(alpha=0.15)
     lasso_reg.fit(X, y)
-    print(lasso_reg.predict(1.5))
+    print(lasso_reg.predict([[1.5]]))
     print(lasso_reg.coef_)
 
     sgd_reg = SGDRegressor(penalty='l1', max_iter=1000)
     sgd_reg.fit(X, y.ravel())
-    print(sgd_reg.predict(1.5))
+    print(sgd_reg.predict([[1.5]]))
     print(sgd_reg.coef_)
 if __name__ == '__main__':
   main()
--- algorithm/linear_regression/ridge_regression.py
+++ algorithm/linear_regression/ridge_regression.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import Ridge
 from sklearn.linear_model import SGDRegressor
 
 __author__ = 'gloom'
-"""这个类是栅格搜索，他们只是来源于不同的两个类，这个方法主要是为了能够在lasso和岭回归中选择到最好方法和参数"""
+"""这个是岭回归，使用L2正则"""
 def main():
     X = 2 * np.random.rand(100, 1)
     y = 4 + 3 * X + np.random.randn(100, 1)
@@ -18,7 +18,7 @@ def main():
     """
     sgd_reg = SGDRegressor(penalty='l2', n_iter=1000)
     sgd_reg.fit(X, y.ravel())
-    print(sgd_reg.predict(1.5))
+    print(sgd_reg.predict([[1.5]]))
     print("W0=", sgd_reg.intercept_)
     print("W1=", sgd_reg.coef_)
 if __name__ == '__main__':
