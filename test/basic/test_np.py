9769fda3ee9b9ca25e5299d58b3e9bd18a5a9afd
quyang <quyang@focusmedia.cn>
2018-12-26 08:31:43 +0800

    2018-12-10 提交了决策树bagging代码

--- test/basic/test_np.py
+++ test/basic/test_np.py
@@ -9,7 +9,14 @@ def main():
     # 这里相当于是随机X维度X1，rand是随机均匀分布
     X =  np.random.rand(100, 1)
     print(np.ones((100, 1)))
+def test_array():
+    result = ['basic_info','gender/female','0.5','262727688','0.5411']
+    result.insert(2, '' if not result[1].__contains__('/') else result[1].split('/')[1])
+    result[1]=result[1] if not result[1].__contains__('/') else result[1].split('/')[0]
+    print(result)
+
 
 if __name__ == '__main__':
-  main()
+    print(list(filter(lambda x: x % 2 == 0, range(1, 21))))
+    test_array()
 
